25f46d5b34cfbe2d474eaed237f60e9b8cc3f947
Ed <et@edwinty.com>
2019-09-24 16:51:14 +0800

    adding blog and jobs apps

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/edwinty/Desktop/Coding/udemy/Django/portfolio/myvenv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
