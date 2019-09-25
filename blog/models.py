0d2e0767664888a547bcf1e3bc8a93165ee923a6
Ed <et@edwinty.com>
2019-09-25 18:09:07 +0800

    adding blog models.py and admin.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class BlogConfig(models.Model):
+    title = models.CharField()
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/blog/')
